8b601d334c67ce3329924139d9ba22aac71ad4f0
Mellivor <siagorat@gmail.com>
2021-11-16 23:08:27 +0200

    Formic start of implementation

---
+++ project_1/my-app/src/components/Content/login/logform.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { useFormik } from "formik";
+// import stl from './Logform.module.css';
+
+const Logform = () => {
+  const formik = useFormik({
+      initialValues: {
+            email: "",
+            password: "",
+      },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        name="password"
+        type="password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Logform;
+
+// function App() {
+//   return <SignupForm />;
+// }
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
--- project_1/my-app/src/components/Content/login/login.jsx
+++ project_1/my-app/src/components/Content/login/login.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Logform from './logform';
 
 const Login = (props) => {
     return (
         <div>
             <h2>Login</h2>
-            <LoginForm/>
+            <Logform/>
         </div>
 
     )
@@ -12,21 +13,21 @@ const Login = (props) => {
 
 export default Login;
 
-const LoginForm = (props) => {
-    return (
-        <form>
-            <div>
-                <input type="text" placeholder ={'Login'} />
-            </div>
-            <div>
-                <input type="text" placeholder={'Pasword'} />
-            </div>
-            <div>
-                <input type="checkbox" name="remember"  />Remember me
-            </div>
-            <div>
-                <button>Login</button>
-            </div>
-        </form>
-    )
-}
+// const LoginForm = (props) => {
+//     return (
+//         <form>
+//             <div>
+//                 <input type="text" placeholder ={'Login'} />
+//             </div>
+//             <div>
+//                 <input type="text" placeholder={'Pasword'} />
+//             </div>
+//             <div>
+//                 <input type="checkbox" name="remember"  />Remember me
+//             </div>
+//             <div>
+//                 <button>Login</button>
+//             </div>
+//         </form>
+//     )
+// }
